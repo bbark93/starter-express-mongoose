e7249cc5d6a70887631a415483834f3a9fb9e551
Brandon Barksdale <bbark93@gmail.com>
2026-03-28 13:28:20 -0400

    .read and .update

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,10 +1,72 @@
 const User = require("./users.model")
 
+async function userExist(req, res, next) {
+  const { userId } = req.params;
+  const foundUser = await User.findOne({ "_id": userId });
+  if (foundUser) {
+    res.locals.user = foundUser;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `User id not found: ${userId} `
+  });
+}
+
+function read(req, res, next) {
+  res.json({ data: res.locals.user });
+}
+
+async function update(req, res) {
+  const user = res.locals.user;
+  const { data: { username, email } = {} } = req.body;
+  user.username = username;
+  user.email = email;
+  await user.save();
+  res.json({ data: user });
+}
+
 async function list(req, res) {
   const users = await User.find()
   res.send(users);
 }
 
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Must include a ${propertyName}`
+    });
+  };
+  
+}
+
+async function create(req, res) {
+  const { data: { username, email } = {} } = req.body;
+  const newUser = new User({
+    username: username,
+    email: email,
+  });
+  await newUser.save();
+  res.status(201).json({ data: newUser });
+};
+
 module.exports = {
+  read: [userExist, read],
   list,
+  create: [
+    bodyDataHas("username"),
+    bodyDataHas("email"),
+    create,
+  ],
+  update: [
+    userExist,
+    bodyDataHas("username"),
+    bodyDataHas("email"),
+    update,
+  ]
 };
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -2,6 +2,8 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./users.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/:userId").get(controller.read).put(controller.update).all(methodNotAllowed);
+
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
